e3b29f6e620f3d4bf8ac7864e48f0da4803957f1
liangyuhit <312592076@qq.com>
2019-08-19 17:48:16 +0200

    2019-8-19 17:48:15

--- Read_File.py
+++ Read_File.py
@@ -41,6 +41,32 @@ def Read_Data_4Ch(name):
     Data_Ch4 = np.array(Data_Ch4)
     return Data_Ch1, Data_Ch2, Data_Ch3, Data_Ch4, Fs
     
+def Read_Data_2Ch(name):
+    '''
+        Return Data in File (4 Channels: Data_Ch1, Data_Ch2, Data_Ch3, Data_Ch4)
+        File name required (default path)
+    '''
+    print('Reading Data')
+    with open(name,'r') as fid:
+        line=''
+        while line[0:4] != '----':
+            line = fid.readline()
+            print(line)
+            if line[0:2] == 'Fs':
+                p, q, m, n = line.strip().split(' ')
+                Fs = float(m)
+                print('Fs = %f\n'%Fs)
+        out_str = fid.readlines()
+    Data_Ch1, Data_Ch2 = [], []
+    for line in out_str:
+        a, b= line.strip().split(', ')
+        Data_Ch1.append(float(a))
+        Data_Ch2.append(float(b))
+    Data_Ch1 = np.array(Data_Ch1)
+    Data_Ch2 = np.array(Data_Ch2)
+    return Data_Ch1, Data_Ch2, Fs
+    
+    
 '''
     平滑滤波
 '''
@@ -52,9 +78,9 @@ def running_mean(x, N):
 '''
     读取数据
 '''
-file_name = 'Cavity_Scanning_28ms.txt'
+file_name = 'Camera Test.txt'
 now = datetime.datetime.now()
-Data_Ch1, Data_Ch2, Data_Ch3, Data_Ch4, Fs = Read_Data_4Ch(file_name)
+Data_Ch1, Data_Ch2, Fs = Read_Data_2Ch(file_name)
 
 Data = Data_Ch1
 N_Data = len(Data)
@@ -63,10 +89,9 @@ T = N_Data*tau0
 print('Sampling Rate: %e'%Fs)
 print('Read length: %i'%N_Data)
 
-Data_Ch1 = running_mean(Data_Ch1, 100)[100:]
-Data_Ch2 = running_mean(Data_Ch2, 100)[100:]
-Data_Ch3 = running_mean(Data_Ch3, 100)[100:]
-Data_Ch4 = running_mean(Data_Ch4, 100)[100:]
+Data_Ch1 = running_mean(Data_Ch1, 100)
+Data_Ch2 = running_mean(Data_Ch2, 100)
+
 
 '''
     可视化
@@ -74,14 +99,14 @@ Data_Ch4 = running_mean(Data_Ch4, 100)[100:]
 if 1:
     plt.figure(1)
         
-    plt.plot(Data_Ch1, label='Reflected Intensity 1', color='yellow')
-    plt.plot(Data_Ch2, label='Reflected Intensity 2', color='cyan')
+    plt.plot(Data_Ch1, label='Trigger', color='yellow')
+    plt.plot(Data_Ch2, label='Flash', color='cyan')
 #     plt.plot(Data_Ch3, label='Demodulated Error Signal 1', color='magenta')
 #     plt.plot(Data_Ch4*2, label='Demodulated Error Signal 2', color='blue')
-    plt.xlim(2e5, 5e5)
-    plt.ylim(0, 3)
-    plt.xlabel('Cavity Length Scanning')
-    plt.ylabel('Voltage [V]')
+#     plt.xlim(2e5, 5e5)
+#     plt.ylim(0, 3)
+#     plt.xlabel('Cavity Length Scanning')
+#     plt.ylabel('Voltage [V]')
     plt.grid(which = 'both')
     plt.legend(loc='center right')
     plt.get_current_fig_manager().window.setGeometry(20, 50, 1000, 800)
--- Read_Scope.py
+++ Read_Scope.py
@@ -24,30 +24,30 @@ def Export_Data(file_name, header, out_str):
 now = datetime.datetime.now()
 scope = Rigol_DS4054.RigolScope('USB0::0x1AB1::0x04B1::DS4A140800046::INSTR')
 print(scope.Read_ID())
-
-Data_Ch1, Data_Ch2, Data_Ch3, Data_Ch4, Memory_Depth, Fs = scope.Read()
+print(scope.Read_Memory_Depth())
+Data_Ch1, Data_Ch2, Memory_Depth, Fs = scope.Read()
 # Data_Ch5 = scope.Read_Ch3 - scope.Read_Ch4
 Data = Data_Ch1
 N_Data, tau0= len(Data), 1.0/Fs  # 采样点数,采样间隔
 T = N_Data*tau0 # 采样时间
 timeline = np.array([x*tau0 for x in range(N_Data)]) #采样序列
-
+ 
 print('Sample Rate: %e'%Fs)
 print('Memory Depth: %i'%Memory_Depth)
 print('Data Length: %i'%N_Data)
-
-
+ 
+ 
 ''' 
     输出数据
 '''
-file_name = 'Demodulation_14ms_16MHz.txt'####################################################################################
+file_name = 'Camera Test.txt'####################################################################################
 header = ['%s\n' %file_name,
       'Local current time : %s\n' %now.strftime("%Y-%m-%d %H:%M:%S"),
       'Fs = %e (Hz)\n' %Fs,##########################################################################################################
       'Data Length: %i\n' %N_Data,############################################################################################
       'Time Scale = %e (s)\n' %T,############################################################################################
-      'Channel_1: Reflected Light 1\n',############################################################################################
-      'Channel_2: Reflected Light 2\n',############################################################################################
+      'Channel_1: Trigger\n',############################################################################################
+      'Channel_2: Flash\n',############################################################################################
       'Channel_3: XXX\n',############################################################################################
       'Channel_4: XXX\n',############################################################################################
 #       'Channel_3: Demodulated S_curve (from CH_No.2)\n',############################################################################################
@@ -55,28 +55,28 @@ header = ['%s\n' %file_name,
 #       'Data_Ch5: Difference between Channel_3 & Channel_4 (Ch3-Ch4)\n'################################################################################
       '-------------------------------------------------\n',
       ]
-out_str = ['%.4f, %.4f, %.4f, %.4f\n' %(Data_Ch1[i], Data_Ch2[i], Data_Ch3[i], Data_Ch4[i]) for i in range(len(Data))] 
+# out_str = ['%.4f, %.4f, %.4f, %.4f\n' %(Data_Ch1[i], Data_Ch2[i], Data_Ch3[i], Data_Ch4[i]) for i in range(len(Data))] 
 # out_str = [' %.4f, %.4f, %.4f, %.4f, %.4f\n' %(Data_Ch1[i], Data_Ch2[i], Data_Ch3[i], Data_Ch4[i], Data_Ch5[i]) for i in range(len(Data))]    
-# out_str = ['%.4f, %.4f, %.4f\n' %(Data_Ch3[i], Data_Ch4[i], Data_Ch5[i]) for i in range(len(Data))]    
-
+out_str = ['%.4f, %.4f\n' %(Data_Ch1[i], Data_Ch2[i]) for i in range(len(Data))]    
+ 
 ''' 
     保存文件
 '''
 Export_Data(file_name, header, out_str)
-
-
+ 
+ 
 '''
     可视化
 '''
 if 1:
     plt.figure(1)
-        
+         
 #     plt.subplot(221)
     #     plt.plot(timeline, Data, color='blue', marker=' ', fillstyle='full', markeredgecolor='blue', markeredgewidth=0.0)
     plt.plot(Data_Ch1, color='yellow')
     plt.plot(Data_Ch2, color='cyan')
-    plt.plot(Data_Ch3, color='magenta')
-    plt.plot(Data_Ch4, color='blue')
+#     plt.plot(Data_Ch3, color='magenta')
+#     plt.plot(Data_Ch4, color='blue')
     plt.xlabel('Time [s]')
     plt.ylabel('Voltage [V]')
     plt.grid(which = 'both')
@@ -88,5 +88,5 @@ if 1:
 #     plt.xlabel('Time [s]')
 #     plt.ylabel('Voltage [V]')
 #     plt.grid(which = 'both')
-    
+     
     plt.show()
